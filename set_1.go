a695a6d8c716a05efa9ba3a9b63ec525a1b01d76
Alessandro Sforzin <alesforz@gmail.com>
2023-09-27 15:23:36 +0200

    renamed hexStrToBase64 to HexToBase64 - added Challenge 2 of Set 1 - Fixed XOR

--- set_1.go
+++ set_1.go
@@ -6,12 +6,43 @@ import (
 	"fmt"
 )
 
-// hexStrToBase64 converts a hexadecimal string to its Base64 representation.
-func hexStrToBase64(hexStr string) (string, error) {
-	hexBytes, err := hex.DecodeString(hexStr)
+// HexToBase64 converts a hexadecimal string to its Base64 representation.
+func HexToBase64(inputHex string) (string, error) {
+	decoded, err := hex.DecodeString(inputHex)
 	if err != nil {
-		return "", fmt.Errorf("malformed input hex string: %s", hexStr)
+		return "", fmt.Errorf("malformed input hex string: %s", inputHex)
 	}
 
-	return base64.StdEncoding.EncodeToString(hexBytes), nil
+	return base64.StdEncoding.EncodeToString(decoded), nil
+}
+
+// XORHexStrings performs a bitwise XOR operation between two input hexadecimal strings
+// of equal length and returns the result as a hexadecimal string.
+func XORHexStrings(inputHex1, inputHex2 string) (string, error) {
+	// We decode the hex strings to bytes before checking their length, because the byte
+	// length might be different from the hex string length due to how hexadecimal
+	// encoding works, and direct string length comparison might not always give you the
+	// correct assessment.
+	decoded1, err := hex.DecodeString(inputHex1)
+	if err != nil {
+		return "", fmt.Errorf("malformed input hex string: %s", inputHex1)
+	}
+
+	decoded2, err := hex.DecodeString(inputHex2)
+	if err != nil {
+		return "", fmt.Errorf("malformed input hex string: %s", inputHex2)
+	}
+
+	if len(decoded1) != len(decoded2) {
+		return "", fmt.Errorf("decoded bytes are of different lengths, but must be equal")
+	}
+
+	// we could reuse decoded1 to save an allocation, but I think creating a separate slice makes
+	// the code clearer.
+	result := make([]byte, len(decoded1))
+	for i := range decoded1 {
+		result[i] = decoded1[i] ^ decoded2[i]
+	}
+
+	return hex.EncodeToString(result), nil
 }
--- set_1_test.go
+++ set_1_test.go
@@ -2,17 +2,33 @@ package main
 
 import "testing"
 
-func TestHexStrToBase64(t *testing.T) {
+func TestHexToBase64(t *testing.T) {
 	var (
 		hexTestStr    = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 		base64WantStr = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 	)
 
-	gotStr, err := hexStrToBase64(hexTestStr)
+	gotStr, err := HexToBase64(hexTestStr)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
 	if gotStr != base64WantStr {
 		t.Errorf("got: %s\nwant: %s\n", gotStr, base64WantStr)
+	}
+}
+
+func TestXORHexStrings(t *testing.T) {
+	var (
+		hexStr1    = "1c0111001f010100061a024b53535009181c"
+		hexStr2    = "686974207468652062756c6c277320657965"
+		hexWantStr = "746865206b696420646f6e277420706c6179"
+	)
+
+	gotStr, err := XORHexStrings(hexStr1, hexStr2)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if gotStr != hexWantStr {
+		t.Errorf("got: %s\nwant: %s\n", gotStr, hexWantStr)
 	}
 }
