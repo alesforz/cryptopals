34c654f2bc81634f006426f18fb8c3b99c0cb7bc
Alessandro Sforzin <alessandro@informal.systems>
2025-02-28 12:29:24 +0100

    add solution to challenge 5 (set 1) to pkg cpxor

--- cpxor/xor.go
+++ cpxor/xor.go
@@ -61,6 +61,26 @@ func decryptSingleByteCipher(cipherText []byte) (string, byte) {
 	return string(plainText), key
 }
 
+// encryptRepeatingKey encrypts the given text using a repeating-key XOR operation.
+// Each byte of the text is XORed with a corresponding byte from the key. If the
+// length of the text exceeds the length of the key, the key is repeated cyclically.
+// For example, if the text is "HELLO" and the key is "AB", the effective key
+// used for encryption would be "ABABA".
+// It returns the encrypted text as a new byte slice.
+// encryptRepeatingKey does not modify the input slices.
+// (Solves challenge 5 of set 1).
+func encryptRepeatingKey(plainText, key []byte) []byte {
+	var (
+		cipherText = make([]byte, len(plainText))
+		keyLen     = len(key)
+	)
+	for i := range plainText {
+		cipherText[i] = plainText[i] ^ key[i%keyLen]
+	}
+
+	return cipherText
+}
+
 // blocks takes two byte slices of equal length, b1 and b2, and returns a new
 // byte slice containing the result of a byte-wise XOR operation between
 // corresponding elements of b1 and b2.
--- cpxor/xor_test.go
+++ cpxor/xor_test.go
@@ -82,6 +82,26 @@ func TestDecryptSingleByteCipher(t *testing.T) {
 
 }
 
+func TestEncryptRepeatingKey(t *testing.T) {
+	const (
+		plainText = `Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`
+
+		key            = "ICE"
+		wantCipherText = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	)
+
+	var (
+		plainTextBytes = []byte(plainText)
+		keyBytes       = []byte(key)
+	)
+
+	gotCipherText := encryptRepeatingKey(plainTextBytes, keyBytes)
+	if hex.EncodeToString(gotCipherText) != wantCipherText {
+		t.Errorf("want: %s\ngot: %s\n", wantCipherText, gotCipherText)
+	}
+}
+
 func TestBlocks(t *testing.T) {
 	t.Run("EqualLength", func(t *testing.T) {
 		var (
