3748492f45f6d17b8ea89024a38d319f4e4c21fa
Alessandro Sforzin <alessandro@informal.systems>
2025-04-04 15:13:13 +0200

    change PKCS7 parameter type to uint8

--- cppad/pkcs7.go
+++ cppad/pkcs7.go
@@ -4,19 +4,13 @@ package cppad
 // of padding to the end of the it.
 // For example, "YELLOW SUBMARINE" (16 bytes) padded to 20 bytes is:
 // "YELLOW SUBMARINE\x04\x04\x04\x04"
-// If targetLen >=256, it will pad to size 255.
 // PKCS7 does not modify the input slice; rather, it returns a new slice with the
 // padded data.
 // (Solves challenge 9 of set 2)
-func PKCS7(data []byte, size int) []byte {
-	if size >= 256 {
-		// can't fit numbers >= 256 in one byte of padding.
-		size = 255
-	}
-
+func PKCS7(data []byte, size uint8) []byte {
 	var (
 		dLen   = len(data)
-		pad    = size - dLen%size
+		pad    = int(size) - dLen%int(size)
 		padded = make([]byte, dLen+pad)
 	)
 	copy(padded, data)
