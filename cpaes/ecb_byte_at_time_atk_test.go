4b627f2a5789636335f1d1f73aa1369261dd313f
Alessandro <alesforz@gmail.com>
2025-05-04 16:14:23 +0200

    add solution to challenge 14 (set 2)

--- cpaes/ecb_byte_at_time_atk_test.go
+++ cpaes/ecb_byte_at_time_atk_test.go
@@ -18,3 +18,18 @@ func TestByteAtTimeAtk(t *testing.T) {
 	t.Log("secret length: ", len(decryptedSecret))
 	t.Logf("secret: %s \n", decryptedSecret)
 }
+
+func TestByteAtTimeAtkWithPrefix(t *testing.T) {
+	oracle, err := ecbEncryptionOracleWithPrefix()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decryptedSecret, err := byteAtTimeAtkWithPrefix(oracle)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("secret length: ", len(decryptedSecret))
+	t.Logf("secret: %s \n", decryptedSecret)
+}
